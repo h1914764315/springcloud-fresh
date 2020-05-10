6c9de8a4d92d691e5563ee6100d3b15d6e950612
hy <1914764315@qq.com>
2020-05-10 15:55:04 +0800

    添加配置中心

---
+++ springcloud-fresh-config/src/main/java/com/hy/springcloud/fresh/config/App.java
@@ -0,0 +1,13 @@
+package com.hy.springcloud.fresh.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
