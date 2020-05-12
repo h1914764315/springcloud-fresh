728bf404af6c2379699c9056ad3130a76592c277
h1914764315 <1914764315@qq.com>
2020-05-12 20:51:30 +0800

    远程读eureka服务器加载

--- springcloud-fresh-eureka-server/src/main/java/com/hy/fresh/eureka/server/EurekaServerApplication.java
+++ springcloud-fresh-eureka-server/src/main/java/com/hy/fresh/eureka/server/EurekaServerApplication.java
@@ -1,15 +1,15 @@
-package com.hy.springcloud.fresh.config.server;
+package com.hy.fresh.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class EurekaServerApplication {
     public static void main( String[] args ){
-    	SpringApplication.run(ConfigServerApplication.class, args);
+    	SpringApplication.run(EurekaServerApplication.class,args);
     }
 }
---
+++ springcloud-fresh-eureka-server/src/main/java/com/hy/fresh/eureka/server/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.hy.fresh.eureka.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	//高版本的spring cloud 会默认启动csf功能, 即跨域访问功能
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/eureka/**");
+	}
+}
